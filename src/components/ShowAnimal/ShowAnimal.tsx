55dfd4c2f4e3a11d6e6f0c390672bf93c34dac14
feliciacalming <felicia.calming@gmail.com>
2023-05-02 17:55:37 +0200

    (feat): add placeholder image

--- src/components/ShowAnimal/ShowAnimal.tsx
+++ src/components/ShowAnimal/ShowAnimal.tsx
@@ -1,11 +1,20 @@
+import { useState } from "react";
 import { IAnimal } from "../../models/IAnimal";
 import "./ShowAnimal.scss";
+import { getFromLS } from "../../helpers/getFromLS";
+import noImageLogo from "../../assets/no-image-icon.png";
 
 export const ShowAnimal = (props: IAnimal) => {
+  const [allAnimals, setAllAnimals] = useState(getFromLS());
+
   return (
     <>
       <div className="animal">
-        <img className="animal__image" src={props.imageUrl}></img>
+        <img
+          className="animal__image"
+          src={props.imageUrl}
+          onError={(e) => (e.currentTarget.src = noImageLogo)}
+        />
         <h3 className="animal__name">{props.name}</h3>
         <p className="animal__desc">{props.shortDescription}</p>
       </div>
