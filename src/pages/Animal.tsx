829303f7e8b7cdf9e10d39ea431a38648d675d68
feliciacalming <felicia.calming@gmail.com>
2023-05-02 16:38:29 +0200

    (feat): add feedAnimal and disabled state for button

--- src/pages/Animal.tsx
+++ src/pages/Animal.tsx
@@ -4,8 +4,13 @@ import { ShowAnimal } from "../components/ShowAnimal/ShowAnimal";
 import { getFromLS } from "../helpers/getFromLS";
 import { IAnimal } from "../models/IAnimal";
 import { Error } from "./Error";
+import { useState } from "react";
+import { saveToLS } from "../helpers/saveToLS";
 
 export const Animal = () => {
+  const [allAnimals, setAllAnimals] = useState<IAnimal[]>(getFromLS());
+  const [disabled, setDisabled] = useState(false);
+
   const params = useParams();
   const animals: IAnimal[] = getFromLS();
 
@@ -13,11 +18,28 @@ export const Animal = () => {
     (animal) => animal.id.toString() === params.id
   );
 
+  const feedAnimal = () => {
+    allAnimals.map((animal) => {
+      if (animal.id.toString() === params.id) {
+        animal.isFed = true;
+        setDisabled(true);
+      }
+    });
+
+    saveToLS(allAnimals);
+  };
+
   return (
     <>
       <Header></Header>
       {chosenAnimal ? (
-        <ShowAnimal {...chosenAnimal}></ShowAnimal>
+        <>
+          {" "}
+          <ShowAnimal {...chosenAnimal}></ShowAnimal>
+          <button onClick={feedAnimal} disabled={disabled}>
+            Mata lilla djuret
+          </button>
+        </>
       ) : (
         <Error></Error>
       )}
