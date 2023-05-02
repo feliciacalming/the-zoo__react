e70886c8495a92d576192f62924cf1981acc6969
feliciacalming <felicia.calming@gmail.com>
2023-05-02 12:27:00 +0200

    (refactor): update Animals.tsx

--- src/pages/Animals.tsx
+++ src/pages/Animals.tsx
@@ -1,3 +1,17 @@
+import { useLoaderData } from "react-router-dom";
+import { IAnimalsLoader } from "../models/IAnimalsLoader";
+import { useState } from "react";
+
 export const Animals = () => {
-  return <></>;
+  const [allAnimals, setallAnimals] = useState([]);
+  const { animals } = useLoaderData() as IAnimalsLoader;
+
+  return (
+    <>
+      {animals.map((animal) => {
+        animal.id;
+      })}
+      ;
+    </>
+  );
 };
