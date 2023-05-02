4a5d09f5f7f9fea082a2cd5ba4808bf037ff0361
feliciacalming <felicia.calming@gmail.com>
2023-05-02 10:09:10 +0200

    (feat): add router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,30 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import App from "./App";
+import "./index.css";
+import { Startpage } from "./pages/Startpage";
+import { Animals } from "./pages/Animals";
+import { Animal } from "./pages/Animal";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Startpage></Startpage>,
+  },
+  {
+    path: "/animals",
+    element: <Animals></Animals>,
+  },
+  {
+    path: "/animals/:id",
+    element: <Animal></Animal>,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
+    <RouterProvider router={router} />
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
