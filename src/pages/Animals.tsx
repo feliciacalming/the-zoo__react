93e3e19ac4eb1a547b446f52bb1aeb0d10b44729
feliciacalming <felicia.calming@gmail.com>
2023-05-02 15:32:52 +0200

    (feat): add useEffect to Animals

--- src/pages/Animals.tsx
+++ src/pages/Animals.tsx
@@ -1,17 +1,30 @@
-import { useLoaderData } from "react-router-dom";
-import { IAnimalsLoader } from "../models/IAnimalsLoader";
-import { useState } from "react";
+import { Header } from "../components/Header/Header";
+import { IAnimal } from "../models/IAnimal";
+import { useEffect, useState } from "react";
+import { getAnimals } from "../services/AnimalService";
+import { ShowAnimal } from "../components/ShowAnimal/ShowAnimal";
+import { Link } from "react-router-dom";
+import { saveToLS } from "../helpers/saveToLS";
 
 export const Animals = () => {
-  const [allAnimals, setallAnimals] = useState([]);
-  const { animals } = useLoaderData() as IAnimalsLoader;
+  const [allAnimals, setallAnimals] = useState<IAnimal[]>([]);
+
+  useEffect(() => {
+    getAnimals().then((animals) => setallAnimals(animals));
+  }, []);
+
+  saveToLS(allAnimals);
 
   return (
     <>
-      {animals.map((animal) => {
-        animal.id;
-      })}
-      ;
+      <Header></Header>
+      <section className="animals-container">
+        {allAnimals.map((animal, index) => (
+          <Link key={index} to={animal.id.toString()}>
+            <ShowAnimal {...animal}></ShowAnimal>
+          </Link>
+        ))}
+      </section>
     </>
   );
 };
