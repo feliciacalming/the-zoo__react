8dd0cd821d455136373767a29192eef266d7559c
feliciacalming <felicia.calming@gmail.com>
2023-05-02 15:27:15 +0200

    (feat): add Header.tsx

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,12 @@
+import "./Header.scss";
+import { Navbar } from "../Navbar/Navbar";
+
+export const Header = () => {
+  return (
+    <>
+      <header>
+        <Navbar></Navbar>
+      </header>
+    </>
+  );
+};
