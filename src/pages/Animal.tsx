e48f117b8d29d172320065233986fb77fb0a25c3
feliciacalming <felicia.calming@gmail.com>
2023-05-02 15:33:22 +0200

    (refactor): update Animal

--- src/pages/Animal.tsx
+++ src/pages/Animal.tsx
@@ -1,3 +1,26 @@
+import { useParams } from "react-router-dom";
+import { Header } from "../components/Header/Header";
+import { ShowAnimal } from "../components/ShowAnimal/ShowAnimal";
+import { getFromLS } from "../helpers/getFromLS";
+import { IAnimal } from "../models/IAnimal";
+import { Error } from "./Error";
+
 export const Animal = () => {
-  return <></>;
+  const params = useParams();
+  const animals: IAnimal[] = getFromLS();
+
+  let chosenAnimal = animals.find(
+    (animal) => animal.id.toString() === params.id
+  );
+
+  return (
+    <>
+      <Header></Header>
+      {chosenAnimal ? (
+        <ShowAnimal {...chosenAnimal}></ShowAnimal>
+      ) : (
+        <Error></Error>
+      )}
+    </>
+  );
 };
