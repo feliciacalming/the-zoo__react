f4277b12917ceca514680156b8d38f102df90fa8
feliciacalming <felicia.calming@gmail.com>
2023-05-03 10:52:50 +0200

    (feat): move saveToLS into if statement

--- src/helpers/checkTime.ts
+++ src/helpers/checkTime.ts
@@ -6,13 +6,12 @@ export const checkTime = (listOfAnimals: IAnimal[]) => {
 
   listOfAnimals.map((animal) => {
     let lastFed = Date.parse(animal.lastFed);
-    if (todaysDate - lastFed > 10000) {
-      console.log(`${animal.name} är hungrig`);
+    if (todaysDate - lastFed > 5000) {
+      // console.log(`${animal.name} är hungrig`);
       animal.isFed = false;
+      saveToLS(listOfAnimals);
     } else {
       console.log(`${animal.name} är MÄTT SOM FAN`);
     }
   });
-
-  saveToLS(listOfAnimals);
 };
