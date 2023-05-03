f48f04d49aa044cd31046d04cfc228873179c503
feliciacalming <felicia.calming@gmail.com>
2023-05-03 10:51:48 +0200

    (fix): fix useEffect bug

--- src/pages/Animals.tsx
+++ src/pages/Animals.tsx
@@ -5,26 +5,33 @@ import { getAnimals } from "../services/AnimalService";
 import { ShowAnimal } from "../components/ShowAnimal/ShowAnimal";
 import { Link } from "react-router-dom";
 import { saveToLS } from "../helpers/saveToLS";
+import { getFromLS } from "../helpers/getFromLS";
 
 export const Animals = () => {
   const [allAnimals, setallAnimals] = useState<IAnimal[]>([]);
+  const animalsFromLS = getFromLS();
 
   useEffect(() => {
-    getAnimals().then((animals) => setallAnimals(animals));
+    if (animalsFromLS.length === 0) {
+      getAnimals().then((animals) => {
+        saveToLS(animals);
+        setallAnimals(animals);
+      });
+    } else {
+      setallAnimals(animalsFromLS);
+    }
   }, []);
-
-  saveToLS(allAnimals);
 
   return (
     <>
       <Header></Header>
-      <section className="animals-container">
+      <main className="main__animals">
         {allAnimals.map((animal, index) => (
           <Link key={index} to={animal.id.toString()}>
             <ShowAnimal {...animal}></ShowAnimal>
           </Link>
         ))}
-      </section>
+      </main>
     </>
   );
 };
