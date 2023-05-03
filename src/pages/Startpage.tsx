036cc17b2b8b07e735550bbbb4c355b86a9fc65b
feliciacalming <felicia.calming@gmail.com>
2023-05-03 10:53:20 +0200

    (feat): add img to startpage

--- src/pages/Startpage.tsx
+++ src/pages/Startpage.tsx
@@ -1,14 +1,16 @@
 import { Header } from "../components/Header/Header";
-import { getFromLS } from "../helpers/getFromLS";
 
 export const Startpage = () => {
-  const animal = getFromLS();
-  console.log(animal);
-
   return (
     <>
       <Header></Header>
-      Välkommen till da zoo!
+      <main className="startpage">
+        <h1>Välkommen!</h1>
+        <img
+          className="start-image"
+          src="https://img.freepik.com/free-vector/group-wild-animals_1308-43813.jpg?w=1800&t=st=1683060233~exp=1683060833~hmac=b9cb2c4f063d730857a95de0ae808b45741f6352fe48982afd5c43705261a520"
+        />
+      </main>
     </>
   );
 };
