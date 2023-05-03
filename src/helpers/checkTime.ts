f6a58f2a242189540df3936dd9205d68dd433079
feliciacalming <felicia.calming@gmail.com>
2023-05-03 13:00:44 +0200

    (feat): update checkTime with timer parameter

--- src/helpers/checkTime.ts
+++ src/helpers/checkTime.ts
@@ -1,14 +1,14 @@
 import { IAnimal } from "../models/IAnimal";
 import { saveToLS } from "./saveToLS";
 
-export const checkTime = (listOfAnimals: IAnimal[]) => {
+export const checkTime = (animals: IAnimal[], timer: number) => {
   const todaysDate = new Date().getTime();
 
-  listOfAnimals.map((animal) => {
+  animals.map((animal) => {
     let lastFed = Date.parse(animal.lastFed);
-    if (todaysDate - lastFed > 180000) {
+    if (todaysDate - lastFed > timer) {
       animal.isFed = false;
-      saveToLS(listOfAnimals);
+      saveToLS(animals);
     }
   });
 };
