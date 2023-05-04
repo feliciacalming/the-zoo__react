a42c3d851de791065343475a0f3ad65a829587e3
feliciacalming <felicia.calming@gmail.com>
2023-05-04 16:40:46 +0200

    (refactor): destructure props in ShowAnimal

--- src/components/ShowAnimal/ShowAnimal.tsx
+++ src/components/ShowAnimal/ShowAnimal.tsx
@@ -1,30 +1,40 @@
-import { useState } from "react";
 import "./ShowAnimal.scss";
 import noImageLogo from "../../assets/no-image-icon.png";
 import { IAnimalProps } from "../../models/IAnimalProps";
 
-export const ShowAnimal = (props: IAnimalProps) => {
+export const ShowAnimal = ({
+  id,
+  name,
+  isFed,
+  imageUrl,
+  shortDescription,
+  longDescription,
+  fullDescription,
+}: IAnimalProps) => {
   return (
     <>
       <div
-        className={`animal ${`animal__${props.id.toString()}`} ${
-          props.isFed ? "" : "animal--hungry"
+        className={`animal ${`animal__${id.toString()}`} ${
+          isFed ? "" : "animal--hungry"
         }`}
       >
-        <img
-          className="animal__image"
-          src={props.imageUrl}
-          onError={(e) => (e.currentTarget.src = noImageLogo)}
-        />
-        <article className="animal__info">
-          <h3 className="animal__name">{props.name}</h3>
-          <p className="animal__desc">{props.shortDescription}</p>
-          {props.fullDescription ? (
-            <p className="animal__desc">{props.longDescription}</p>
-          ) : (
-            ""
-          )}
-        </article>
+        {" "}
+        <div className="animal__section-one">
+          <img
+            className="animal__image"
+            src={imageUrl}
+            onError={(e) => (e.currentTarget.src = noImageLogo)}
+          />
+          <article className="animal__info">
+            <h3 className="animal__name">{name}</h3>
+            <p className="animal__desc">{shortDescription}</p>
+          </article>
+        </div>
+        {fullDescription ? (
+          <p className="animal__desc--long">{longDescription}</p>
+        ) : (
+          ""
+        )}
       </div>
     </>
   );
