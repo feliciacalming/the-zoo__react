9f11b76b6a34591edfeaafdc234db02258a47b7b
feliciacalming <felicia.calming@gmail.com>
2023-05-05 12:31:29 +0200

    (feat): add Footer

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,12 +1,14 @@
 import "./Layout.scss";
 import { Outlet } from "react-router-dom";
 import { Header } from "../Header/Header";
+import { Footer } from "../Footer/Footer";
 
 export const Layout = () => {
   return (
     <>
       <Header></Header>
       <Outlet></Outlet>
+      <Footer></Footer>
     </>
   );
 };
