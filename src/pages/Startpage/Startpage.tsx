74918e304b9351d7a15aa7a564258df7b0b988dc
feliciacalming <felicia.calming@gmail.com>
2023-05-05 12:31:54 +0200

    (feat): add hungryAnimals count to Startpage

--- src/pages/Startpage/Startpage.tsx
+++ src/pages/Startpage/Startpage.tsx
@@ -1,13 +1,27 @@
 import { Header } from "../../components/Header/Header";
+import { checkTime } from "../../helpers/checkTime";
+import { getFromLS } from "../../helpers/getFromLS";
 
 export const Startpage = () => {
+  let numberOfAnimals: number = 15;
+  let hungryAnimals: number = 0;
+  let animalsInLS = getFromLS();
+
+  if (animalsInLS.length > 0) {
+    checkTime(animalsInLS, 14400000);
+    animalsInLS.map((animal) => {
+      if (!animal.isFed) {
+        hungryAnimals++;
+      }
+    });
+
+    numberOfAnimals = hungryAnimals;
+  }
+
   return (
     <>
       <main className="main__startpage">
-        <img
-          className="start-image"
-          src="https://www.torebrings.se/bilder/artikelbilder/17/17230mega.jpg"
-        />
+        <h1>Nu är {numberOfAnimals} djur hungriga</h1>
       </main>
     </>
   );
