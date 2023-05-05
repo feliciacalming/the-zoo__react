498f9c7a33f4a133ce37d55bdb33c00f7656f27f
feliciacalming <felicia.calming@gmail.com>
2023-05-05 11:09:12 +0200

    (refactor): minor styling changes

--- src/pages/Startpage.tsx
+++ src/pages/Startpage.tsx
@@ -5,10 +5,9 @@ export const Startpage = () => {
     <>
       <Header></Header>
       <main className="startpage">
-        <h1>Välkommen!</h1>
         <img
           className="start-image"
-          src="https://img.freepik.com/free-vector/group-wild-animals_1308-43813.jpg?w=1800&t=st=1683060233~exp=1683060833~hmac=b9cb2c4f063d730857a95de0ae808b45741f6352fe48982afd5c43705261a520"
+          src="https://www.torebrings.se/bilder/artikelbilder/17/17230mega.jpg"
         />
       </main>
     </>
