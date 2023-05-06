1d11413570d65415ec4d6fbe5bf0356a515fe113
feliciacalming <felicia.calming@gmail.com>
2023-05-06 18:00:56 +0200

    (feat): add logo to header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,18 @@
 import "./Header.scss";
 import { Navbar } from "../Navbar/Navbar";
+import { Link } from "react-router-dom";
 
 export const Header = () => {
   return (
     <>
       <header>
         <Navbar></Navbar>
+        <div className="logo-wrapper">
+          <Link to="/">
+            {" "}
+            <span className="logo">THE ZOO</span>
+          </Link>
+        </div>
       </header>
     </>
   );
