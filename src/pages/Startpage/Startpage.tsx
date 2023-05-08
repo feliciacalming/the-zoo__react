c42bef4350f7ea1c692d68f9ca9590753f82c9b9
feliciacalming <felicia.calming@gmail.com>
2023-05-08 10:19:40 +0200

    (feat): add useEffect for window resizing

--- src/pages/Startpage/Startpage.tsx
+++ src/pages/Startpage/Startpage.tsx
@@ -1,11 +1,23 @@
-import { Header } from "../../components/Header/Header";
+import "./Startpage.scss";
 import { checkTime } from "../../helpers/checkTime";
 import { getFromLS } from "../../helpers/getFromLS";
+import panda from "../../assets/Panda-waving.gif";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 export const Startpage = () => {
+  const [width, setWidth] = useState(window.innerWidth);
+  const animalsInLS = getFromLS();
   let numberOfAnimals: number = 15;
   let hungryAnimals: number = 0;
-  let animalsInLS = getFromLS();
+
+  useEffect(() => {
+    const handleResizeWindow = () => setWidth(window.innerWidth);
+    window.addEventListener("resize", handleResizeWindow);
+    return () => {
+      window.removeEventListener("resize", handleResizeWindow);
+    };
+  }, []);
 
   if (animalsInLS.length > 0) {
     checkTime(animalsInLS, 14400000);
@@ -20,8 +32,19 @@ export const Startpage = () => {
 
   return (
     <>
-      <main className="main__startpage">
-        <h1>Nu är {numberOfAnimals} djur hungriga</h1>
+      <main className="startpage">
+        <section className="startpage__text">
+          {window.innerWidth > 768 ? <h1>Välkommen till The Zoo!</h1> : <></>}
+          <h2>Här bor ett gäng snälla djur.</h2>
+          <h3>Just nu är {numberOfAnimals} djur hungriga!</h3>
+          <button>
+            <Link to="/animals">
+              Klicka här för att se vilka som behöver matas.
+            </Link>
+          </button>
+        </section>
+        <img className="startpage__image" src={panda} />
+        {window.innerWidth < 768 ? <h1>Välkommen till The Zoo!</h1> : <></>}
       </main>
     </>
   );
